20c0b33238360793f3109c8a60169ae89ee29569
Shubhangi Pagar <Shubhangi_Pagar@symantec.com>
2016-09-04 19:24:54 +0530

    Adding source code

---
+++ src/com/pathak/yuva/Welcome.java
@@ -0,0 +1,30 @@
+
+/*
+ * SYMANTEC: Copyright (c) 2016 Symantec Corporation. All rights reserved.
+ *
+ * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF
+ * SYMANTEC CORPORATION.  USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED
+ * WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF SYMANTEC CORPORATION.
+ * 
+ * The Licensed Software and Documentation are deemed to be commercial
+ * computer software as defined in FAR 12.212 and subject to restricted
+ * rights as defined in FAR Section 52.227-19 "Commercial Computer Software
+ * - Restricted Rights" and DFARS 227.7202, Rights in "Commercial Computer
+ * Software or Commercial Computer Software Documentation," as applicable,
+ * and any successor regulations, whether delivered by Symantec as on
+ * premises or hosted services.  Any use, modification, reproduction
+ * release, performance, display or disclosure of the Licensed Software and
+ * Documentation by the U.S. Government shall be solely in accordance with
+ * the terms of this Agreement.
+ */
+
+package com.pathak.yuva;
+  
+/**
+ * TODO: Update with a detailed description of the interface/class.
+ *
+ */
+public class Welcome {
+
+}
+
